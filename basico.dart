bee43eff651e60ed44c21f232823190eb9e28fec
Ewillianc <herickfilho6@gmail.com>
2023-01-17 22:43:46 -0300

    Praticando uma operadores comuns , entradas e saidas na linguagem Dart.

---
+++ basico.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: unused_local_variable
+
+import 'dart:io';
+
+main() {
+
+  print('Operadores Dart');
+  
+  int n1, n2, m;
+  
+  print('Informe o primeiro numero: ');
+   String? s = stdin.readLineSync();
+ 
+   n1 = int.parse(s!);
+
+  print('Informe o primeiro numero: ');
+   s = stdin.readLineSync();
+  
+   n2 = int.parse(s!);
+
+  print('$n1 + $n2 = ${n1 + n2} ');
+  print('$n1 - $n2 = ${n1 - n2} ');
+  print('$n1 * $n2 = ${n1 * n2} ');
+  print('$n1 / $n2 = ${n1 / n2} ');
+  print('$n1 % $n2 = ${n1 % n2} ');
+  print('$n1 ~/ $n2 = ${n1 ~/ n2} ');
+
+  /*
+   String? s = stdin.readLineSync();
+    if(s!= null){
+      n1 = int.parse(s);
+    }
+  */ 
+
+}
