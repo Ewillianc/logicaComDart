4fe9fb2cd1fb79f73e2b8ba68c4d40a1cc5c4d3b
Ewillianc <herickfilho6@gmail.com>
2023-01-19 22:54:33 -0300

    operadores relacionais e Logicos

--- basico.dart
+++ basico.dart
@@ -3,20 +3,19 @@
 import 'dart:io';
 
 main() {
-
   print('Operadores Dart');
-  
+
   int n1, n2;
-  
-  print('Informe o primeiro numero: ');
-   String? s = stdin.readLineSync();
- 
-   n1 = int.parse(s!);
 
   print('Informe o primeiro numero: ');
-   s = stdin.readLineSync();
-  
-   n2 = int.parse(s!);
+  String? s = stdin.readLineSync();
+
+  n1 = int.parse(s!);
+
+  print('Informe o primeiro numero: ');
+  s = stdin.readLineSync();
+
+  n2 = int.parse(s!);
 // Operadores simples
 
   print('$n1 + $n2 = ${n1 + n2} ');
@@ -32,8 +31,8 @@ main() {
     if(s!= null){
       n1 = int.parse(s);
     }
-  */ 
-  //Comparaçã0 e Operadores logicos.
+  */
+  //operadores relacionais e Logicos
 
   int a, b, c;
 
@@ -41,15 +40,47 @@ main() {
   b = 3;
   c = 5;
 
+  print("Relacionais");
   print(a > b);
   print(a == b);
   print(a != b);
   print(a <= 2);
   print(c == a + b);
-  print(b<=c%2);
+  print(b <= c % 2);
 
-  //Logicos
+  print(" Logicos ");
+  print((a == b) || (c > a));
+  print((a == b) & (c > a));
+  print(!((a == b) & (c > a)));
 
-  
+  /*  Ordem de precedência 
+  () > ^ > *,/ > Relacionais  da direita para a esquerda > & > || > !.
+  */
+
+  //Triangulo
+  int l2 = 0;
+  bool eQ, eS, tri;
+
+  print('Primeiro Lado: ');
+  s = stdin.readLineSync();
+  int l1 = int.parse(s!);
+
+  print('Segundo Lado: ');
+  //outra entrada na pratica.
+  s = stdin.readLineSync();
+  if (s != null) {
+    l2 = int.parse(s);
+  }
+  print('Terceiro Lado: ');
+
+  s = stdin.readLineSync();
+  int l3 = int.parse(s!);
+
+  tri = (l1 < (l2 + l3)) & (l2 < (l1 + l3)) & (l3 < (l1 + l2));
+  eQ = (l1 == l2) & (l2 == l3);
+  eS = (l1 != l2) & (l2 != l3) & (l1 != l3) & tri;
+
+  print('O triangulo e EQUILATERO ? $eQ');
+  print('O triangulo e ESCALENO ? $eS');
 
 }
